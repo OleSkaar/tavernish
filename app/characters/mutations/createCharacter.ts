d545481d0058ca0049aab4f97b15884b310a28ca
unknown <olejohska@gmail.com>
2021-07-14 00:16:26 +0200

    Validation for abilities on character

--- app/characters/mutations/createCharacter.ts
+++ app/characters/mutations/createCharacter.ts
@@ -1,6 +1,7 @@
 import { resolver } from "blitz"
 import db from "db"
 import { z } from "zod"
+import { AbilityRank } from "@prisma/client"
 
 export const CreateCharacter = z.object({
   userId: z.number(),
@@ -8,6 +9,15 @@ export const CreateCharacter = z.object({
   flaw: z.string().optional(),
   titles: z.string().optional(),
   rank: z.string().optional(),
+  abilities: z
+    .array(
+      z.object({
+        name: z.string(),
+        ranking: z.nativeEnum(AbilityRank),
+        isBioAbility: z.boolean(),
+      })
+    )
+    .optional(),
 })
 
 export default resolver.pipe(resolver.zod(CreateCharacter), resolver.authorize(), async (input) => {
